1439ff36d3249d5c355c5d64be6c8932f184eb67
Amusah <henryamusah@gmail.com>
2023-02-12 16:37:17 +0000

    added a for loop

--- excercise_1/script.js
+++ excercise_1/script.js
@@ -1,8 +1,15 @@
-let str = '#'
+let str = '#';
 let counter = 0;
 
+/*
 while(counter < 7){
     console.log(str);
     counter += 1;
+    str += '#';
+}
+*/
+
+for (let count = 0; count < 7; count++){
+    console.log(str);
     str += '#';
 }
