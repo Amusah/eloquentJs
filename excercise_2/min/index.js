b1486c86f7ce5c72cb16af789f419e7966a0e1bc
Amusah <henryamusah@gmail.com>
2023-03-09 13:09:10 +0000

    updated Math.min excercise

--- excercise_2/min/index.js
+++ excercise_2/min/index.js
@@ -1,2 +1,13 @@
-let min = (a, b) => Math.min(a, b);
-console.log(min(85, 99));
+// let min = (a, b) => Math.min(a, b);
+// console.log(min(85, 99));
+
+let min = (z, y) =>{
+    if (z > y) return z;
+    else return y
+}
+console.log(min(9, 898));
+
+// let min = (z, y) =>{
+//     return z > y ? z : y;
+// }
+// console.log(min(9, 4))
