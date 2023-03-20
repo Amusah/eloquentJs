6adcd43814b27a88a2be4909434bfd81faa2529d
Amusah <henryamusah@gmail.com>
2023-03-20 16:07:19 +0000

    completed range and sum assignment

--- excercise_3/sum_of_range/index.js
+++ excercise_3/sum_of_range/index.js
@@ -1,12 +1,18 @@
 const range = (start, end, step = 1) => {
     let result = [];
     //step = -Infinity;
-    for(let i = start; i <= end; i+=step){
-        result.push(i);
+    if(step > 0){
+        for(let i = start; i <= end; i+=step){
+            result.push(i);
+        }
+    }else{
+        for(let i = start; i >= end; i+=step){
+            result.push(i);
+        }
     }
     return result;
 }
-console.log(range(1, 10, 2));
+console.log(range(10, 2, -2));
 
 const sum = arr => {
     let result = 0;
