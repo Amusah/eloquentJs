b52559b425c674755509b2a35e1aa416ae3f2f48
Amusah <henryamusah@gmail.com>
2023-03-21 12:18:25 +0000

    arrayToList

--- excercise_3/list/index.js
+++ excercise_3/list/index.js
@@ -1,3 +1,4 @@
+/*
 const arrayToList = arr => {
     return list = {
         value: arr[0],
@@ -9,4 +10,13 @@ const arrayToList = arr => {
         }
     }
 }
-console.log(arrayToList([1,2,3]))
+*/
+
+const arrayToList = arr => {
+    let list = null;
+    for(let i = arr.length - 1; i >= 0; i--){
+        list = {value: arr[i], rest: list};
+    }
+    return list;
+}
+console.log(arrayToList([1,2,3,6,4]));
