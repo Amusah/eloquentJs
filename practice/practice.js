90b6c497380a9d92da3244392345901ca1670cf9
Amusah <henryamusah@gmail.com>
2023-05-12 00:53:12 +0000

    recognizing texts

--- practice/practice.js
+++ practice/practice.js
@@ -1618,3 +1618,27 @@ let roseDragon = '🌹🐉';
 for(let emoji of roseDragon){
   console.log(emoji);
 }
+
+function flatenArry(array){
+  return array.reduce((acc, script) => {
+    return acc.concat(script.ranges.flat())
+  }, []);
+}
+console.log(flatenArry(SCRIPTS));
+
+// Recognizing Text
+console.log('Recognizing Text');
+function countBy(items, groupName){
+  let counts = [];
+  for(let item of items){
+    let name = groupName(item);
+    let known = counts.findIndex(c => c.name == name);
+    if(known == -1){
+      counts.push({name, count: 1})
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+console.log(countBy([1,2,3,4,5], n => n > 2));
