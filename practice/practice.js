2253ef2fdfed9efed1ae4a424144640ee166ef1d
Amusah <henryamusah@gmail.com>
2023-12-21 02:11:18 +0000

    Matrix class

--- practice/practice.js
+++ practice/practice.js
@@ -1933,3 +1933,92 @@ class Matrix {
     this.content[y * this.width + x] = value;
   }
 }
+
+class MatrixIterator {
+  constructor(matrix){
+    this.x = 0;
+    this.y = 0;
+    this.matrix = matrix;
+  }
+
+  next(){
+    if(this.y == this.matrix.height) return {done: true};
+
+    let value = {x: this.x,
+                 y: this.y, 
+                 value: this.matrix.get(this.x, this.y)};
+
+    this.x++;
+    if(this.x == this.matrix.width){
+      this.x = 0;
+      this.y++;
+    }
+    return {value, done: false};
+  }
+}
+
+Matrix.prototype[Symbol.iterator] = function(){
+  return new MatrixIterator(this);
+}
+
+let matrix = new Matrix(2, 2, (x, y) => `value ${x}, ${y}`);
+for(let {x, y, value} of matrix){
+  console.log(x, y, value);
+}
+
+console.log(' ');
+console.log('//Getters, Setters, and Statics');
+
+let varyingSize = {
+  get size(){
+    return Math.floor(Math.random() * 100);
+  }
+};
+console.log(varyingSize.size);
+
+// setter
+class Temperature {
+  constructor (celsius){
+    this.celsius = celsius;
+  }
+  get fahrenheit(){
+    return this.celsius * 1.8 + 32;
+  }
+  set fahrenheit(value){
+    this.celsius = (value - 32) / 1.8;
+  }
+  static fromFahrenheit(value){
+    return new Temperature((value - 32) / 1.8);
+  }
+}
+
+let temp = new Temperature(22);
+console.log(temp.fahrenheit)
+
+temp.fahrenheit = 86;
+console.log(temp.celsius);
+
+let newTemp = Temperature.fromFahrenheit(100);
+console.log(newTemp);
+
+console.log(' ');
+console.log('// Inheritance');
+
+class SymmetricMatrix extends Matrix{
+  constructor(size, element = (x, y) => undefined){
+    super(size, size, (x, y) => {
+      if(x < y) return element(y, x);
+      else return element(x, y)
+    });
+  }
+
+  set(x, y, value){
+    super.set(x, y, value);
+    if(x != y){
+      super.set(y, x, value)
+    }
+  }
+}
+
+let matrix2 = new SymmetricMatrix(5, (x, y) => `${x}, ${y}`);
+console.log(matrix2.get(2, 3));
