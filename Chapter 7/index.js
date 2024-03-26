91f57380064caaec96800d901b8958c04fba2705
Amusah <henryamusah@gmail.com>
2024-03-26 19:08:58 +0000

    Mail truck's route and Pathfinding

--- Chapter 7/index.js
+++ Chapter 7/index.js
@@ -60,6 +60,7 @@ class VillageState{
 let first = new VillageState('Post Office', [{place: 'Post Office', address: 'Alice\'s House'}]);
 let next = first.move('Alice\'s House');
 
+console.log(next)
 console.log(next.place);
 console.log(next.parcels);
 console.log(first.place);
@@ -113,3 +114,49 @@ VillageState.random = function(parcelCount = 5){
 }
 
 runRobot(VillageState.random(), randomRobot);
+
+
+// The Mail Truck's Route
+console.log('/*************** Mail Truck\'s Route *****************/');
+
+const mailRoute = [
+  'Alice\'s House', 'Cabin', 'Alice\'s House', 'Bob\'s House',
+  'Town Hall', 'Daria\'s House', 'Ernie\'s House',
+  'Grete\'s House', 'Shop', 'Grete\'s House', 'Farm',
+  'Marketplace', 'Post Office'
+];
+
+function routeRobot(state, memory){
+  if(memory.length == 0){
+    memory = mailRoute;
+  }
+  return {direction: memory[0], memory: memory.slice(1)};
+}
+
+// PATHFINDING
+console.log('/*************** Pathfinding *****************/');
+
+function findRoute(graph, from, to){
+  let work = [{at: from, route: []}];
+  for(let i = 0; i < work.length; i++){
+    let {at, route} = work[i];
+    for(let place of graph[at]){
+      if(place == to) return route.concat(place);
+      if(!work.some(w => w.at == place)) {
+        work.push({at: place, route: route.concat(place)});
+      }
+    }
+  }
+}
+
+function goalOrientedRobot({place, parcels}, route){
+  if(route.length == 0) {
+    let parcel = parcels[0];
+    if(parcel.place != place){
+      route = findRoute(roadGraph, place, parcel.place);
+    } else{
+      route = findRoute(roadGraph, place, parcel.address )
+    }
+  }
+  return {direction: route[0], memory: route.slice(1)}
+}
