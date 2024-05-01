8c128a8575ec7b3579d9b00ead6305c0f858c4b3
Amusah <henryamusah@gmail.com>
2024-05-01 07:31:28 +0000

    Debugging, Error Propagation, Exceptions

--- Chapter-8/index.js
+++ Chapter-8/index.js
@@ -37,3 +37,75 @@ test('convert Latin text to uppercase', () => {
   return 'hello'.toUpperCase() == 'HELLO';
 });
 
+test('convert Greek text to uppercase', () => {
+  return 'Xaipete'.toUpperCase() == 'XAIPETE';
+});
+
+// DEBUGGING
+console.log(" ");
+console.log("// Debugging");
+
+function numberToString(n, base = 10){
+  let result = "", sign = "";
+  if(n < 0){
+    sign = '-';
+    n = -n;
+  }
+  do{
+    result = String(n % base) + result;
+    n /= base;
+  } while (n > 0);
+  return sign + result;
+}
+
+console.log(numberToString(13, 10));
+
+// ERROR PROPAGATION
+console.log(" ");
+console.log("// Error Propagation");
+
+/*
+  function promptNumber(question){
+    let result = Number(prompt(question));
+    if (Number.isNaN(result)) return null;
+    else return result;
+  }
+  console.log(promptNumber('How many trees do you see?'));
+
+*/
+
+function lastElement(...array){
+  if(array.length == 0){
+    return {failed: true};
+  } else {
+    return {element: array[array.length - 1]}
+  }
+}
+
+console.log(lastElement(8,9,5,4,2));
+
+
+// EXCEPTIONS
+console.log(" ");
+console.log("// Exceptions");
+
+function promptDirection(question){
+  let result = prompt(question);
+  if(result.toLocaleLowerCase() == 'left') return 'L';
+  if(result.toLocaleLowerCase() == 'right') return 'R';
+  throw new Error(`Invalid direction: ${result}`);
+}
+
+function look(){
+  if(promptDirection('Which way?') == 'L'){
+    return 'a house'
+  } else{
+    return 'two angry bears';
+  }
+}
+
+try {
+  console.log('You see', look());
+} catch (error) {
+  console.log('Something went wrong: ' + error);
+}
